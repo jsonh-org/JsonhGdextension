397ace92efea26819103be229ec493316d51968e
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-04-18 15:08:01 +0100

    Remove unnecessary extension API

--- bat_dump_extension_api.bat
+++
@@ -1,6 +0,0 @@
-:: Prevent quit on error
-if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit
-
-:: Dump extension_api.json
-set godot="C:/Users/patri/Documents/Programs/Godot_v4.4.1-stable_mono_win64/Godot_v4.4.1-stable_mono_win64.exe"
-%godot% --dump-extension-api
