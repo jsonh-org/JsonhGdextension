fd39069465dffda963b8bbbc614cbe75a8bdfb96
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-04-18 14:51:54 +0100

    Fix register_types.h

--- src/register_types.h
+++ src/register_types.h
@@ -1,7 +1,8 @@
-#ifndef EXAMPLE_REGISTER_TYPES_H
-#define EXAMPLE_REGISTER_TYPES_H
+#pragma once
 
-void initialize_gdextension_types();
-void uninitialize_gdextension_types();
+#include <godot_cpp/core/class_db.hpp>
 
-#endif // EXAMPLE_REGISTER_TYPES_H
+using namespace godot;
+
+void initialize_gdextension_types(ModuleInitializationLevel p_level);
+void uninitialize_gdextension_types(ModuleInitializationLevel p_level);
