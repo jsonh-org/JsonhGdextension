99e0a956343d6058370b0748bdf195996ae0f018
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-06-05 19:32:18 +0100

    Update update bat

--- bat_update_godot_cpp.bat
+++ bat_update_godot_cpp.bat
@@ -2,7 +2,8 @@
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit
 
 :: Update godot-cpp submodule (https://stackoverflow.com/a/5828561)
+set godot_cpp_branch=4.4
 cd godot-cpp
 git fetch
-git checkout 4.4
-git pull origin 4.4
+git checkout %godot_cpp_branch%
+git pull origin %godot_cpp_branch%
