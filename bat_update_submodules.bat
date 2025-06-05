f90791ba0654026cf2d7cf3b10e87f7ae91af745
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-06-05 18:44:31 +0100

    Update submodules

--- bat_update_submodules.bat
+++ bat_update_submodules.bat
@@ -1,5 +1,8 @@
 :: Prevent quit on error
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit
 
-:: Update submodules
-git submodule update --init
+:: Update submodules (https://stackoverflow.com/a/5828561)
+cd godot-cpp
+git fetch
+git checkout master
+git pull origin master
